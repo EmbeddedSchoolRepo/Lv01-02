c42c16e3fc2535fea231c0e9650d27c66e5a8e35
TaeinPark <pti1360@naver.com>
2021-03-19 21:03:06 +0900

    1회차 숙제 완료(박태인)

---
+++ TaeinPark/homework/c/01/data_type_byte.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//%d를 %lu로 바꾸라는 워닝 메시지가 발생함
+	//7개를 바꿔야 하는데 일일히 바꾸기 귀찮음 어떻게 해야 할까?
+	//:%s/%d/%lu/g -> 모든 %d를 찾아서 %lu로 치환해 주세요.
+	//		모든 -> /g
+	//		치환 -> :%s
+	//		치환 대상 -> %d
+	//		치환 결과 -> %lu
+	//sizeof()는 데이터 타입의 크기를 바이트 단위로 표현해 줍니다.
+	
+        printf("sizeof(char) = %lu\n", sizeof(char));
+	printf("sizeof(short) = %lu\n", sizeof(short));
+        printf("sizeof(int) = %lu\n", sizeof(int));
+        printf("sizeof(long long int) = %lu\n", sizeof(long long int));
+        
+	printf("sizeof(float) = %lu\n", sizeof(float));
+        printf("sizeof(double) = %lu\n", sizeof(double));
+        printf("sizeof(long double) = %lu\n", sizeof(long double));
+
+	return 0;
+
+}
