0c005a2daf7b0fbf1b739ace0476dcde9c704235
silenc3502 <gcccompil3r@gmail.com>
2021-04-17 12:39:23 +0900

    구조체 버전 벡터 연산

--- SanghoonLee/Lectures/c/06/struct.c
+++ SanghoonLee/Lectures/c/06/struct.c
@@ -1,6 +1,13 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// typedef를 통해 struct test를 test로 축약시킴
+// 길게 쓰기 싫어서 적는다고 봐도 된다.
+// 사실 좀 더 깊은 의미가 있는데
+// 자바의 인터페이스 역할도 할 수 있긴하다.
+typedef struct test test;
 
 struct address
 {
@@ -9,9 +16,13 @@ struct address
 	int zipcode;
 };
 
+// { 10,     주소      } ----------> { 20, 주소 } -> { 30, 주소 }
+//  int, 구조체 포인터                이하 동문        이하 동문
+
 struct test
 {
 	int data;
+	// 자료구조
 	struct test *self_pointer;
 	struct address addr;
 	// 함수 포인터 - 다형성
@@ -37,15 +48,30 @@ void print_test(void)
 	printf("print test\n");
 }
 
-void init_test_struct(struct test *sample)
+void init_test_struct(struct test *sample, test *link)
 {
-	sample->data = 7;
-	sample->self_pointer = NULL;
+	// -> 연산자: 간접 참조 연산으로 구조체 연산에만 활용됨
+	// 구조체가 포인터로 주어졌을 경우 해당 포인터를 통해 구조체 필드에 접근한다면
+	// -> 화살표 연산자를 사용해야함
+
+	// . 연산자: 직접 참조 연산으로 마찬가지로 구조체에서 활용됨
+	// 구조체가 포인터가 아닌 형태로 주어졌을 경우 사용함
+	// 구조체 내부의 멤버 필드에 접근하기 위해 사용됨
+	sample->data = rand() % 10 + 1;
+	sample->self_pointer = link;
 
 	char city[] = "Seoul";
 	char street[] = "마포구 큰우물로 76";
 
+	// 포인터 변수는 메모리를 할당해야 사용할 수 있다.
+	// Heap 메모리는 일반 Stack이나 다른 메모리들과 다르게
+	// 여러 프로세스들이 구동되면서 서로 얽힐 수도 있는데
+	// +1을 통해 NULL 문자를 할당해두면
+	// 뒤에 있는 내용을 붙어서 읽지 않고 끝이 어디인지 정확하게 알 수 있음
 	sample->addr.city = (char *)malloc(strlen(city) + 1);
+	// 위의 메모리 할당 없이 strcpy만 하는 경우 오류
+	// sample->addr.city에 위의 city 배열을 복사한다.
+	// string copy의 약자: strcpy
 	strcpy(sample->addr.city, city);
 
 	sample->addr.street = (char *)malloc(strlen(street) + 1);
@@ -72,6 +98,12 @@ void print_test_struct(struct test sample)
 	printf("print_test = 0x%x\n", print_test);
 }
 
+void clear_malloc(test sample)
+{
+	free(sample.addr.city);
+	free(sample.addr.street);
+}
+
 int main(void)
 {
 	// 구조체를 사용하는 방법
@@ -85,9 +117,27 @@ int main(void)
 	// 중요한 것은 C언어에서 구조체는
 	// 우리만의 전용 데이터타입을 만드는 방식이란 것이다.
 	struct test sample;
+	// int sample
+	// float sample
+	// double sample
+	// char *sample
+	// char **sample
+	// int *****sample
+	// double ****************************sample
+	test sample2;
 
-	init_test_struct(&sample);
+	srand(time(NULL));
+
+	init_test_struct(&sample, NULL);
 	print_test_struct(sample);
+	printf("\n");
+
+	init_test_struct(&sample2, &sample);
+	print_test_struct(sample2);
+	printf("sample address = 0x%x\n", &sample);
+
+	clear_malloc(sample);
+	clear_malloc(sample2);
 
 	return 0;
 }
